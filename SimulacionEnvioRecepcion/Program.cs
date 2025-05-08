1398fde0b507f5a659f011981ce1049a8babf40b
Ragnar7Lothbrok <kiqqe.7@gmail.com>
2025-05-08 14:39:51 +0200

    Descifrar clave simetrica

--- SimulacionEnvioRecepcion/Program.cs
+++ SimulacionEnvioRecepcion/Program.cs
@@ -65,7 +65,11 @@ namespace SimuladorEnvioRecepcion
                 //LADO RECEPTOR
 
                 //Descifrar clave simétrica
+                ClaveSimetricaReceptor.Key = Receptor.DescifrarMensaje(ClaveSimetricaKeyCifrada);
+                ClaveSimetricaReceptor.IV = Receptor.DescifrarMensaje(ClaveSimetricaIVCifrada);
 
+                Console.WriteLine("Clave simétrica descifrada (Key): {0}", BytesToStringHex(ClaveSimetricaReceptor.Key));
+                Console.WriteLine("Clave simétrica descifrada (IV): {0}", BytesToStringHex(ClaveSimetricaReceptor.IV));
                 
                 //Descifrar clave simétrica
  
